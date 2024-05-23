85304d2bc431a64cf6226a50b2934a1ec88fd512
Henokagb <henok.agbodjogbe@epitech.eu>
2024-05-23 10:44:14 +0100

    week1 case1

---
+++ week1/case1.sql
@@ -0,0 +1,11 @@
+-- 1. What is the total amount each customer spent at the restaurant?
+SELECT
+    customer_id, SUM(price)
+ FROM
+ 	dannys_diner.sales FULL JOIN
+    dannys_diner.members
+ 	USING(customer_id)
+ 	FULL JOIN
+    dannys_diner.menu
+ 	USING(product_id)
+GROUP BY customer_id
