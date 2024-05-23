12719d9f24f47652de9e9949cd60a9a505b9f331
Henokagb <henok.agbodjogbe@epitech.eu>
2024-05-23 11:53:19 +0100

    case2

---
+++ week1/case2.sql
@@ -0,0 +1,9 @@
+-- 2. How many days has each customer visited the restaurant?
+SELECT
+    customer_id, COUNT(order_date)
+ FROM
+ 	dannys_diner.sales FULL JOIN
+    dannys_diner.members
+ 	USING(customer_id)
+GROUP BY customer_id
+ORDER BY customer_id
