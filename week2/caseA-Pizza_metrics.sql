8d367dba123aef63d8cf0402329f3572b30d4b23
Henokagb <henok.agbodjogbe@epitech.eu>
2024-05-30 10:44:24 +0100

    Clean the data

--- week2/caseA-Pizza_metrics.sql
+++ week2/caseA-Pizza_metrics.sql
@@ -1,3 +1,28 @@
+--Deal with null, NaN and empty values
+UPDATE pizza_runner.customer_orders
+SET exclusions = NULL
+WHERE exclusions = 'null' OR exclusions = '' OR exclusions = 'NaN';
+
+UPDATE  pizza_runner.customer_orders
+SET extras = NULL
+WHERE extras = 'null' OR extras = '' OR extras = 'NaN';
+
+UPDATE pizza_runner.runner_orders
+SET pickup_time = NULL
+WHERE pickup_time = 'null' OR pickup_time = '' OR pickup_time = 'NaN';
+
+UPDATE  pizza_runner.runner_orders
+SET distance = NULL
+WHERE distance = 'null' OR distance = '' OR distance = 'NaN';
+
+UPDATE  pizza_runner.runner_orders
+SET duration = NULL
+WHERE duration = 'null' OR duration = '' OR duration = 'NaN';
+
+UPDATE  pizza_runner.runner_orders
+SET cancellation = NULL
+WHERE cancellation = 'null' OR cancellation = '' OR cancellation = 'NaN';
+
 --1 How many pizzas were ordered?
 
 SELECT COUNT(pizza_id) 
