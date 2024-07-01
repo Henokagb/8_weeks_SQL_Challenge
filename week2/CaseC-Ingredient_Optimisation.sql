59480c118d2ca49b971f2fd6902e0efb549a45f0
Henokagb <henok.agbodjogbe@epitech.eu>
2024-07-01 07:55:48 +0100

    Case C4

--- week2/CaseC-Ingredient_Optimisation.sql
+++ week2/CaseC-Ingredient_Optimisation.sql
@@ -43,7 +43,7 @@ WHERE duration LIKE '%mins';
 WITH pizza_toppings AS 
 	(
     SELECT pizza_id, UNNEST(string_to_array) AS topping
-	FROM (
+	  FROM (
 			SELECT pizza_id, string_to_array(toppings, ',')::int[]
 			FROM pizza_runner.pizza_recipes
   			) AS arrayed
@@ -85,10 +85,53 @@ FROM (
 			WHERE exclusions IS NOT NULL
   			) AS exclu
   )
-  
+
   SELECT pt.topping_name, exclu_id, COUNT(exclu_id)
   FROM exclu_added, pizza_runner.pizza_toppings AS pt
   WHERE pt.topping_id = exclu_id
   GROUP BY exclu_id, pt.topping_name
   ORDER BY count DESC
   LIMIT 1
+
+--4 Generate an order item for each record in the customers_orders table in the format of one of the following:
+/*
+Meat Lovers
+Meat Lovers - Exclude Beef
+Meat Lovers - Extra Bacon
+Meat Lovers - Exclude Cheese, Bacon - Extra Mushroom, Peppers
+*/
+
+WITH
+new_arrayed AS (
+SELECT order_id, pizza_id, string_to_array(exclusions, ',')::int[] AS exclusion, 					string_to_array(extras, ',')::int[] AS extra
+ FROM pizza_runner.customer_orders
+ ORDER BY order_id
+  ),
+  
+ unarrayed AS (
+    SELECT order_id, pizza_id, UNNEST(exclusion) AS exclusion, UNNEST(extra) AS extra
+    FROM new_arrayed
+    	UNION ALL
+    SELECT  order_id, pizza_id, exclusions::INTEGER, extras::INTEGER
+    FROM pizza_runner.customer_orders
+    WHERE exclusions IS NULL AND extras IS NULL
+     
+  ),
+
+pre_items AS (
+	SELECT order_id,
+ 				(
+                  SELECT pizza_name FROM pizza_runner.pizza_names WHERE pizza_id = unarrayed.pizza_id
+                ),
+ 				(
+                SELECT topping_name AS exclusion FROM pizza_runner.pizza_toppings WHERE topping_id = unarrayed.exclusion
+                 ),      
+				(
+                SELECT topping_name AS extra FROM pizza_runner.pizza_toppings WHERE topping_id = unarrayed.extra
+                 )
+ 	FROM unarrayed
+)
+
+SELECT order_id, pizza_name, ' - Exclude' || ' '||  string_agg(exclusion, ', ') AS exclusion, ' - Extra' || ' '|| string_agg(extra, ', ') AS extra
+FROM pre_items 
+GROUP BY order_id, pizza_name
